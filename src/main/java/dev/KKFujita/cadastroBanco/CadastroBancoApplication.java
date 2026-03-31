8abd9d27ba295475c1d1256347f656509590ec6b
Kauã Kyuzo Fujita <271039221+KauaFuijtakkyz@users.noreply.github.com>
2026-03-31 15:53:20 -0300

    first commit

---
+++ src/main/java/dev/KKFujita/cadastroBanco/CadastroBancoApplication.java
@@ -0,0 +1,13 @@
+package dev.KKFujita.cadastroBanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroBancoApplication.class, args);
+	}
+
+}
