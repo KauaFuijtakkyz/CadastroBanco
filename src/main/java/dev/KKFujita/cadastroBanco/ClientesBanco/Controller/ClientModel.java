91218ec507173b0452844c191669d1e78281a81c
Kauã Kyuzo Fujita <271039221+KauaFuijtakkyz@users.noreply.github.com>
2026-03-31 17:02:27 -0300

    feat: Configurado o ClientModel

--- src/main/java/dev/KKFujita/cadastroBanco/ClientesBanco/Controller/ClientModel.java
+++ src/main/java/dev/KKFujita/cadastroBanco/ClientesBanco/Controller/ClientModel.java
@@ -1,4 +1,43 @@
 package dev.KKFujita.cadastroBanco.ClientesBanco.Controller;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "tb_cadastro")
 public class ClientModel {
+
+    @Column (name = "nome")
+    private String nome;
+
+    @Column (name = "idade")
+    private int idade;
+
+    @Column (unique = true)
+    private int CPF;
+
+    @Column (unique = true)
+    private int RG;
+
+    @Column (unique = true)
+    private String email;
+
+    @Column (name = "numeroTefelone")
+    private int numeroTelefone;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private Long id;
+
+
+
+
+
 }
