4c65a00edace322b9a9966440b9ab10b22b64d3d
Kauã Kyuzo Fujita <271039221+KauaFuijtakkyz@users.noreply.github.com>
2026-03-31 17:06:12 -0300

    feat: Configurado o ClientDTO e ClientMapper

--- src/main/java/dev/KKFujita/cadastroBanco/ClientesBanco/Controller/ClientDTO.java
+++ src/main/java/dev/KKFujita/cadastroBanco/ClientesBanco/Controller/ClientDTO.java
@@ -1,4 +1,19 @@
 package dev.KKFujita.cadastroBanco.ClientesBanco.Controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClientDTO {
+
+    private Long id;
+    private String nome;
+    private int idade;
+    private String email;
+    private int RG;
+    private int CPF;
+    private int numeroTelefone;
 }
--- src/main/java/dev/KKFujita/cadastroBanco/ClientesBanco/Controller/ClientMapper.java
+++ src/main/java/dev/KKFujita/cadastroBanco/ClientesBanco/Controller/ClientMapper.java
@@ -1,4 +1,31 @@
 package dev.KKFujita.cadastroBanco.ClientesBanco.Controller;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClientMapper {
+    public ClientModel map(ClientDTO clientDTO){
+        ClientModel clientModel = new ClientModel();
+        clientModel.setId(clientDTO.getId());
+        clientModel.setNome(clientDTO.getNome());
+        clientModel.setRG(clientDTO.getRG());
+        clientModel.setCPF(clientDTO.getCPF());
+        clientModel.setIdade(clientDTO.getIdade());
+        clientModel.setEmail(clientDTO.getEmail());
+        clientModel.setNumeroTelefone(clientDTO.getNumeroTelefone());
+
+        return clientModel;
+    }
+    public ClientDTO map(ClientModel clientModel){
+        ClientDTO clientDTO = new ClientDTO();
+        clientDTO.setId(clientModel.getId());
+        clientDTO.setNome(clientModel.getNome());
+        clientDTO.setRG(clientModel.getRG());
+        clientDTO.setCPF(clientModel.getCPF());
+        clientDTO.setIdade(clientModel.getIdade());
+        clientDTO.setEmail(clientModel.getEmail());
+        clientDTO.setNumeroTelefone(clientModel.getNumeroTelefone());
+
+        return clientDTO;
+    }
 }
