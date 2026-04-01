c91d28e96c50269b0f41bd452cd30248df9f4759
Kauã Kyuzo Fujita <271039221+KauaFuijtakkyz@users.noreply.github.com>
2026-04-01 13:47:34 -0300

    fix: altera o metodo PUT para PATCH

--- src/main/java/dev/KKFujita/cadastroBanco/ClientesBanco/Controller/ClientController.java
+++ src/main/java/dev/KKFujita/cadastroBanco/ClientesBanco/Controller/ClientController.java
@@ -1,15 +1,65 @@
 package dev.KKFujita.cadastroBanco.ClientesBanco.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.KKFujita.cadastroBanco.ClientesBanco.Service.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/clientes")
 public class ClientController {
 
+private final ClientService clientService;
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
     @GetMapping("/boasvindas")
-    public String BoasVindas() {
-        return "Boas vindas.";
+    @Operation(summary = "Mensagem de boas vindas", description = "essa rota da uma mensagem de boas vindas para quem acessa ela")
+    public String boasvindas(){
+        return "Boas vindas";
+    }
+    @PostMapping("/adicionar")
+    public ResponseEntity<String> Adicionarcliente(@RequestBody ClientDTO cliente) {
+        ClientDTO clienteAdd = clientService.adicionarcliente(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Cliente adicionado com sucesso: " + clienteAdd.getNome() + " (ID): " + clienteAdd.getId());
+    }
+    @GetMapping("/listar")
+    public ResponseEntity<List<ClientDTO>> listaclientes() {
+        List<ClientDTO> listarclientes = clientService.listaclientes();
+        return ResponseEntity.ok(listarclientes);}
+
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<?> listarclientesid(@PathVariable Long id) {
+        ClientDTO clientDTO = clientService.listarclientesid(id);
+        if (clientDTO != null){
+            return ResponseEntity.ok(clientDTO);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("não existe cliente com esse ID");
+        }
+    }
+    @PatchMapping("/alterar/{id}")
+    public ResponseEntity<String> alterarclienteporid(@PathVariable Long id,@RequestBody ClientDTO  clienteatualizado) {
+        if (clientService.listarclientesid(id) != null){
+            clientService.atualizarcliente(id, clienteatualizado);
+            return ResponseEntity.ok("Cliente com id: " + id + " Alterado com sucesso");
+        }else {return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("o cliente com esse ID não existe");}
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deletarclienteporid(@PathVariable Long id) {
+        if (clientService.listarclientesid(id) != null) {
+            clientService.deletarclienteporid(id);
+            return ResponseEntity.ok("Cliente com ID: " + id + " Deletado com sucesso");
+        }else {return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("o cliente com esse ID não existe");
+        }
     }
 }
--- src/main/java/dev/KKFujita/cadastroBanco/ClientesBanco/Repository/ClientRepository.java
+++ src/main/java/dev/KKFujita/cadastroBanco/ClientesBanco/Repository/ClientRepository.java
@@ -3,5 +3,5 @@ package dev.KKFujita.cadastroBanco.ClientesBanco.Repository;
 import dev.KKFujita.cadastroBanco.ClientesBanco.Controller.ClientModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClientesRepository extends JpaRepository<ClientModel, Long> {
+public interface ClientRepository extends JpaRepository<ClientModel, Long> {
 }
